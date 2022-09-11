e71b68f8d30a98f7cb78aaf372097b23d29ccb20
jalhagosipo <gpfla757@naver.com>
2022-09-11 16:47:32 +0900

    feat: order 추가

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import tacos.Ingredient;
 import tacos.Taco;
@@ -53,4 +54,9 @@ public class DesignTacoController {
                 .collect(Collectors.toList());
     }
 
+    @PostMapping
+    public String processDesign(Taco design) {
+        log.info("Processing design: " + design);
+        return "redirect:/orders/current";
+    }
 }
