286c2e192bac9d2c6eb65610351f223dec4975e2
jalhagosipo <gpfla757@naver.com>
2022-10-15 22:31:18 +0900

    feat: postTaco 추가

--- src/main/java/tacos/web/api/DesignTacoController.java
+++ src/main/java/tacos/web/api/DesignTacoController.java
@@ -35,4 +35,10 @@ public class DesignTacoController {
         }
         return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
+
+    @PostMapping(consumes="application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Taco postTaco(@RequestBody Taco taco) {
+        return tacoRepo.save(taco);
+    }
 }
