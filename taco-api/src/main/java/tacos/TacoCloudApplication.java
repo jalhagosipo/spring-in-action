ddf5819b94a5096444132089e26ef8aeb8bc6abb
jalhagosipo <gpfla757@naver.com>
2022-11-13 14:51:11 +0900

    chore: move submodule - api

--- taco-api/src/main/java/tacos/TacoCloudApplication.java
+++ taco-api/src/main/java/tacos/TacoCloudApplication.java
@@ -1,15 +1,9 @@
 package tacos;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
 import tacos.Ingredient.Type;
 import tacos.data.IngredientRepository;
 
